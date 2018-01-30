2c120a4ec801601e7d7aaa72ae7fc09f9d474221
Ouhan You <ouhan.you@duke.edu>
2018-01-30 11:20:07 -0500

    connected frontend with RSVP app and migrated models

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['oy5','vcm-2827.vm.duke.edu']
 # Application definition
 
 INSTALLED_APPS = [
+    'RSVP.apps.RsvpConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+    path('RSVP/', include('RSVP.urls')),
     path('admin/', admin.site.urls),
-    path('login/', include('login.urls'),
 ]
