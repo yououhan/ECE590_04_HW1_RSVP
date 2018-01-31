3713d2142622bcfa7f6522e524debd2eaed89323
Ouhan You <ouhan.you@duke.edu>
2018-01-31 18:50:25 -0500

    admin register

--- mysite/RSVP/admin.py
+++ mysite/RSVP/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import People, Event, Question, RegisterEvent, Response, EventAccess, QuestionAccess
+
+admin.site.register(People)
+admin.site.register(Event)
+admin.site.register(Question)
+admin.site.register(RegisterEvent)
+admin.site.register(Response)
+admin.site.register(EventAccess)
+admin.site.register(QuestionAccess)
