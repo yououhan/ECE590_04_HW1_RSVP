92020f8ae975ab9cd6fa292e85ead79a38ae3454
Ouhan You <ouhan.you@duke.edu>
2018-02-01 22:12:43 -0500

    reset database

--- mysite/RSVP/models.py
+++ mysite/RSVP/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.postgres.fields import ArrayField
 from django.utils import timezone
 
 QUESTION_TEXT_MAX_LENGTH = 200
@@ -27,7 +26,8 @@ REGISTER_STATE_CHOICES = (
     ('1', 'Passed'),
     ('2', 'Declined and unread'),
     ('3', 'Declined and read')
-)    
+)
+
 # Create your models here.
 class People(models.Model):
     username = models.CharField(max_length = PEOPLE_USERNAME_MAX_LENGTH, unique = True)
@@ -43,6 +43,7 @@ class Event(models.Model):
     creator = models.ForeignKey(
         People,
         null = True,
+#        limit_choices_to = RegisterEvent(identity = '0'),
         on_delete=models.SET_NULL#Set the reference to NULL (requires the field to be nullable). For instance, when you delete a User, you might want to keep the comments he posted on blog posts, but say it was posted by an anonymous (or deleted) user.
     )
     event_time = models.DateTimeField('event held time', default = timezone.now)
@@ -50,6 +51,23 @@ class Event(models.Model):
     last_updated_time = models.DateTimeField('last updated time', auto_now = True)
     def __str__(self):
         return self.event_name
+    
+class RegisterEvent(models.Model):
+    event = models.ForeignKey(#same event can not be registered twice by the same people!!!
+        Event,
+        on_delete=models.CASCADE
+        )
+    people = models.ForeignKey(
+        People,
+        on_delete=models.CASCADE
+        )
+    class Meta:
+        unique_together = (("event", "people"),)
+    register_time = models.DateTimeField('time registered', auto_now_add = True, null = True)
+    identity = models.CharField(max_length = 1, choices = IDENTITY_CHOICES, null = True)
+    register_state = models.CharField(max_length = 1, choices = REGISTER_STATE_CHOICES)
+    def __str__(self):
+        return '%s registers %s as %s' % (self.people.username, self.event.event_name, self.identity.__str__())
 
 class Question(models.Model):
     event = models.ForeignKey(
@@ -72,54 +90,49 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(
         Question,
-        on_delete=models.CASCADE#When the referenced object is deleted, also delete the objects that have references to it (When you remove a blog post for instance, you might want to delete comments as well).
+        on_delete=models.CASCADE,#When the referenced object is deleted, also delete the objects that have references to it (When you remove a blog post for instance, you might want to delete comments as well).
+        limit_choices_to = {'question_type' : 'S' or 'M'},
     )
     choice_text = models.CharField(max_length = CHOICE_TEXT_MAX_LENGTH)
     def __str__(self):
         return self.choice_text
 
-class RegisterEvent(models.Model):
-    event = models.ForeignKey(#same event can not be registered twice by the same people!!!
-        Event,
-        on_delete=models.CASCADE
-        )
-    people = models.ForeignKey(
-        People,
-        on_delete=models.CASCADE
-        )
-    register_time = models.DateTimeField('time registered', auto_now_add = True)
-    identity = models.CharField(max_length = 1, choices = IDENTITY_CHOICES)
-    register_state = models.CharField(max_length = 1, choices = REGISTER_STATE_CHOICES)
-    def __str__(self):
-        return '%s registers %s as %s' % (self.people.username, self.event.event_name, self.identity)
-
 class MultiChoicesResponse(models.Model):
     question = models.ForeignKey(
         Question,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        limit_choices_to = {'question_type' : 'S' or 'M'},
         )
-    guest = models.ForeignKey(
-        People,
-        on_delete=models.CASCADE
+    register_event = models.ForeignKey(
+        RegisterEvent,
+        on_delete=models.CASCADE,
+        limit_choices_to = {'identity' :'2'},
+        null = True
         )
+    class Meta:
+        unique_together = (("question", "register_event"),)
     answer = models.CharField(max_length = MULTICHOICES_RESPONSE_MAX_LENGTH)
     last_updated_time = models.DateTimeField('last updated time')
     def __str__(self):
-        return '%s reponsed to %s' % (self.guest.username, self.question.question_text)
+        return '%s reponsed to %s' % (self.register_event.people, self.question.question_text)
     
 class TextResponse(models.Model):
     question = models.ForeignKey(
         Question,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        limit_choices_to = {'question_type' : 'T'},
         )
-    guest = models.ForeignKey(
-        People,
-        on_delete=models.CASCADE
+    register_event = models.ForeignKey(
+        RegisterEvent,
+        on_delete=models.CASCADE,
+        limit_choices_to = {'identity' :'2'},
         )
+    class Meta:
+        unique_together = (("question", "register_event"),)
     answer = models.TextField(max_length = TEXT_RESPONSE_MAX_LENGTH)
     last_updated_time = models.DateTimeField('last updated time')
     def __str__(self):
-        return '%s reponsed to %s' % (self.guest.username, self.question.question_text)
+        return '%s reponsed to %s' % (self.register_event.people, self.question.question_text)
 
 class EventAccess(models.Model):
     registerEvent = models.ForeignKey(
