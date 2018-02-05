543168e3ac9e8aacfcaffc7886009a8666ec9227
Ouhan You <ouhan.you@duke.edu>
2018-02-04 23:48:50 -0500

    add a sign up page using django.contrib.auth.views

--- mysite/RSVP/urls.py
+++ mysite/RSVP/urls.py
@@ -1,4 +1,7 @@
-from django.urls import path
+from django.urls import path,include
+from django.conf.urls import url
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -7,5 +10,7 @@ urlpatterns = [
     path('home/<int:people_id>', views.home, name='home'),
     path('event/<int:event_id>', views.events_list, name='events_list'),
     path('index', views.index, name = 'index'),
-    path('sign_up', views.sign_up, name = 'sign_up')
+    path('sign_up', views.sign_up, name = 'sign_up'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='RSVP/login.html')),
+    #    path('accounts/', auth_views.login, name = 'login'),
 ]
